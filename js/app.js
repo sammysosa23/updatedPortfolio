f03275c2d88c37de6529af7102e1ef55f0a7aa6c
Sam <sam.terrazas@yahoo.com>
2019-01-27 20:19:44 -0500

    updated app.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // INITIALIZE SIDENAV 
     $('.button-collapse').sideNav();
 
-    // Init Modal
+    // INITIALIZE MODAL
     $('.modal').modal();
 
     // SCROLLFIRE
@@ -34,62 +34,3 @@ $(document).ready(function () {
 
 });
 
-$(document).ready(function () {
-    // Init Sidenav
-    $('.button-collapse').sideNav();
-
-    // Init Modal
-    $('.modal').modal();
-
-    // Scrollfire
-    const options = [
-        {
-            selector: '.row-1', offset: 50, callback: function (el) {
-                Materialize.fadeInImage($(el));
-            }
-        },
-        {
-            selector: '.row-2', offset: 300, callback: function (el) {
-                Materialize.fadeInImage($(el));
-            }
-        },
-        {
-            selector: '.row-3', offset: 400, callback: function (el) {
-                Materialize.fadeInImage($(el));
-            }
-        }
-    ];
-
-    Materialize.scrollFire(options);
-
-});
-
-$(document).ready(function () {
-    // Init Sidenav
-    $('.button-collapse').sideNav();
-
-    // Init Modal
-    $('.modal').modal();
-
-    // Scrollfire
-    const options = [
-        {
-            selector: '.row-1', offset: 50, callback: function (el) {
-                Materialize.fadeInImage($(el));
-            }
-        },
-        {
-            selector: '.row-2', offset: 300, callback: function (el) {
-                Materialize.fadeInImage($(el));
-            }
-        },
-        {
-            selector: '.row-3', offset: 400, callback: function (el) {
-                Materialize.fadeInImage($(el));
-            }
-        }
-    ];
-
-    Materialize.scrollFire(options);
-
-});
